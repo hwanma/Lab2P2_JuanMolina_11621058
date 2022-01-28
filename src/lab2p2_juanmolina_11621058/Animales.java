11dd9835ad28918f7756e5b289d6c82a9292f857
hwanma <98614936+hwanma@users.noreply.github.com>
2022-01-28 13:57:52 -0600

    second commit

---
+++ src/lab2p2_juanmolina_11621058/Animales.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2p2_juanmolina_11621058;
+
+/**
+ *
+ * @author Hwan
+ */
+public class Animales {
+    
+}
